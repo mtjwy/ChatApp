baa2d9819e595e047a9e4e30a0af158c7505c24c
Yu Wu <wuyucsu@gmail.com>
2015-05-31 01:43:54 -0500

    Client(can both send and read); Use a Thread to read message from server.

--- ChatApp/src/main/java/com/mtjwy/ChatApp/SimpleChatClient.java
+++ ChatApp/src/main/java/com/mtjwy/ChatApp/SimpleChatClient.java
@@ -3,17 +3,24 @@ package com.mtjwy.ChatApp;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 public class SimpleChatClient {
+	JTextArea incoming;
 	JTextField outgoing;
+	BufferedReader reader;
 	PrintWriter writer;
 	Socket sock;
 	
@@ -34,20 +41,37 @@ public class SimpleChatClient {
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(new SendButtonListener());
         
-        mainPanel.add(outgoing);
-        mainPanel.add(sendButton);
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        //setUpNetworking();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setEditable(false);
         
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        
+        mainPanel.add(outgoing);
+        mainPanel.add(qScroller);
+        mainPanel.add(sendButton);
+        
+        setUpNetwroking();
+        
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+        
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setSize(650, 500);
         frame.setVisible(true);
 	}
 	
-	private void setUpNextWroking() {
+	
+	
+	private void setUpNetwroking() {
 		//make a Socket, then make a PrintWriter
 		//assign the PrintWriter to writer instance variable
 		try {
 			sock = new Socket(serverIP, serverPort);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+			reader = new BufferedReader(streamReader);
 			writer = new PrintWriter(sock.getOutputStream());
 			System.out.println("networking established");
 		} catch (IOException e) {
@@ -71,6 +95,22 @@ public class SimpleChatClient {
 		}
 		
 	}
+	
+	
+	class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("client read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
 	
 	
 }
