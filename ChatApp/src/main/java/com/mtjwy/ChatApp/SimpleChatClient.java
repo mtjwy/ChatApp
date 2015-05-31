d4db6a294dec02ed791fc0fdad82576cd8fe0409
Yu Wu <wuyucsu@gmail.com>
2015-05-31 01:19:42 -0500

    GUI implemented

--- ChatApp/src/main/java/com/mtjwy/ChatApp/SimpleChatClient.java
+++ ChatApp/src/main/java/com/mtjwy/ChatApp/SimpleChatClient.java
@@ -1,10 +1,14 @@
 package com.mtjwy.ChatApp;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class SimpleChatClient {
@@ -12,9 +16,27 @@ public class SimpleChatClient {
 	PrintWriter writer;
 	Socket sock;
 	
+	public static void main(String[] args) {
+        new SimpleChatClient().go();
+    }
+	
 	public void go() {
 		//make gui and register a listener with the send button
 		//call the setUpNetworking() method
+		JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        //setUpNetworking();
+        
+        frame.setSize(650, 500);
+        frame.setVisible(true);
 	}
 	
 	private void setUpNextWroking() {
@@ -31,6 +53,8 @@ public class SimpleChatClient {
 		}
 		
 	}
+	
+	
 }
 
 
